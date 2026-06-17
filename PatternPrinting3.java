cb3bf914f35609f521286e7c6d70eb77d070eb58
Chandru <gurusaranchandru@gmail.com>
2026-06-17 17:08:54 +0530

    Add files via upload

---
+++ PatternPrinting3.java
@@ -0,0 +1,22 @@
+package chapter2;
+
+public class PatternPrinting3 {
+
+	public static void main(String[] args) {
+		  int n = 5;
+
+	        for (int i = 0; i < n; i++) {
+	            // Print spaces
+	            for (int j = 0; j < i; j++) {
+	                System.out.print(" ");
+	            }
+
+	            // Print star
+	            System.out.println("*");
+	        }
+	    }
+	
+
+	}
+
+
